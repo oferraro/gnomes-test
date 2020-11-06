5a4ac1184b00702738e61a0e4a9af0cd57c80051
oferraro <oscar@oferraro.com>
2020-11-06 15:31:40 +0100

    organize set active show/hide details in a method

--- src/components/gnome.tsx
+++ src/components/gnome.tsx
@@ -23,34 +23,31 @@ export class Gnome extends React.Component<GnomeProps, GnomeState> {
         return professionsList.join(', ');
     }
 
+    setActive(gnomeId: number) {
+        const newState = (`gnome-${gnomeId}` === this.state.showDetails)
+            ? '' : `gnome-${gnomeId}`;
+        this.setState({...this.state, showDetails: newState});
+    }
+
     render() {
         return <>
             <div key={this.props.gnome.id} className="gnome-card">
                 <div>
                     <img src={this.props.gnome.thumbnail} className="gnome-pic" />
                 </div>
-                <strong> {this.props.gnome.name} </strong>
-                {
-                    // TODO: Use a component fot the thumbnail
-                } <br />
+                <div>
+                    <strong> {this.props.gnome.name} </strong>
+                </div>
                 {showProperty({title: 'Age', data: this.props.gnome.age})}
-                {showProperty({title: 'Weight', data: this.props.gnome.weight})}
-                {showProperty({title: 'Height', data: this.props.gnome.height})}
+                {showProperty({title: 'Weight', data: this.props.gnome.weight.toFixed(2)})}
+                {showProperty({title: 'Height', data: this.props.gnome.height.toFixed(2)})}
                 {showProperty({title: 'Hair Color', data: this.props.gnome.hair_color})}
 
-                <div onClick={() => {
-                    const newState = (`gnome-${this.props.gnome.id}` === this.state.showDetails)
-                        ? '' : `gnome-${this.props.gnome.id}`;
-                    this.setState({...this.state, showDetails: newState});
-                }}>
+                <div onClick={() => this.setActive(this.props.gnome.id)}>
                     <u className="with-pointer">Show Details</u>
                     <div className={ this.state.showDetails === `gnome-${this.props.gnome.id}` ? '' : 'hidden'}>
-                        <strong>professions</strong>: {
-                            this.formatStrings(this.props.gnome.professions)
-                        } <br />
-                        <strong>friends</strong>: {
-                            this.formatStrings(this.props.gnome.friends)
-                        }
+                        {showProperty({title: 'Professions', data: this.formatStrings(this.props.gnome.professions)})}
+                        {showProperty({title: 'Friends', data: this.formatStrings(this.props.gnome.friends)})}
                     </div>
                 </div>
             </div>
