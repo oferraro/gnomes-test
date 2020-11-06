bbf177e6d78301fefd5f94bccc9cf73704ad04c7
oferraro <oscar@oferraro.com>
2020-11-06 13:21:34 +0100

    First step, get data from API (simulated with json file)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {gnomes: [], didFetch: false}; // TODO: Add gnomo type
+  }
+
+  componentDidMount() {
+    if (!this.state.didFetch) {
+      console.log('fetch');
+      fetch(`${window.location.href}data/data.json`)
+          .then(res => res.json())
+          .then(results => {
+            let allGnomes: any[] = [];
+            results.Brastlewark.forEach((result: any) => { // TODO: use type here
+              allGnomes.push(result);
+            });
+            this.setState({gnomes: allGnomes});
+          }).finally(() => {
+        this.setState({didFetch: true});
+      });
+    }
+  }
+
+  render() {
+
+    return (
+        <div className="App">
+          here
+        {
+          this.state.gnomes.map((gnome: any) => {
+            return <div>
+              id: {gnome.id} <br />
+              name: {gnome.name}  <br />
+              thumbnail: {gnome.thumbnail}  <br />
+              age: {gnome.age}  <br />
+              weight: {gnome.weight}  <br />
+              height: {gnome.height}  <br />
+              hair_color: {gnome.hair_color}  <br />
+              professions":["Metalworker","Woodcarver","Stonecarver"," Tinker","Tailor","Potter"],
+              "friends":["Cogwitz Chillwidget","Tinadette Chillbuster
+              <hr />
+            </div>
+          })
+        }
+        </div>
+    );
+  }
 }
 
 export default App;
